0caad95d5939dc070351fc74c2a6d4f1562d49fb
sona mittal <sonamittal285@gmail.com>
2025-12-22 12:56:50 +0530

    i

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,10 +5,15 @@ import { db } from "@/lib/databse/db-connect"; // your drizzle instance
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-    // or "mysql", "sqlite"
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE__CLIENT_SECRET as string,
+    },
   },
   trustedOrigins: ["http://localhost:3000"],
 });
