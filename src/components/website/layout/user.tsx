ee1b8ebf199ffefd71741f8728352da23ac37096
sona mittal <sonamittal285@gmail.com>
2026-02-05 18:24:26 +0530

    changes

--- src/components/website/layout/user.tsx
+++ src/components/website/layout/user.tsx
@@ -1,36 +1,36 @@
-"use client";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+// "use client";
+// import { authClient } from "@/lib/auth-client";
+// import { useRouter } from "next/navigation";
 
-const User = () => {
-  const { data, isPending, error, refetch } = authClient.useSession();
+// const User = () => {
+//   const { data, isPending, error, refetch } = authClient.useSession();
 
-  const router = useRouter();
-  const handleLogout = async () => {
-    try {
-      await authClient.signOut();
-      router.push("/auth/signin");
-    } catch (error) {
-      console.error("Logout failed:", error);
-    }
-  };
+//   const router = useRouter();
+//   const handleLogout = async () => {
+//     try {
+//       await authClient.signOut();
+//       router.push("/auth/signin");
+//     } catch (error) {
+//       console.error("Logout failed:", error);
+//     }
+//   };
 
-  return (
-    <>
-      <div className="flex items-center justify-between px-9 py-4 mt-3">
-        <h2 className="text-xl text-white font-bold">Home Page</h2>
-        {data && (
-          <button
-            onClick={handleLogout}
-            type="button"
-            className="text-white bg-[#9a4141] font-medium px-6 py-1.5 rounded-sm cursor-pointer"
-          >
-            Logout
-          </button>
-        )}
-      </div>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <div className="flex items-center justify-between px-9 py-4 mt-3">
+//         <h2 className="text-xl text-white font-bold">Home Page</h2>
+//         {data && (
+//           <button
+//             onClick={handleLogout}
+//             type="button"
+//             className="text-white bg-[#9a4141] font-medium px-6 py-1.5 rounded-sm cursor-pointer"
+//           >
+//             Logout
+//           </button>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
 
-export default User;
+// export default User;
