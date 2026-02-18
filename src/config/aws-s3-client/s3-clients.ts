23e7db971942a1a66d284cf528a6ac9e199967f1
sona mittal <sonamittal285@gmail.com>
2026-02-19 03:49:46 +0530

    s3-client

---
+++ src/config/aws-s3-client/s3-clients.ts
@@ -0,0 +1,28 @@
+import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
+
+const s3Client = () => {
+  const {
+    CLOUDFLARE_R2_ACCOUNT_ID,
+    CLOUDFLARE_R2_ACCESS_KEY_ID,
+    CLOUDFLARE_R2_SECRET_ACCESS_KEY,
+  } = process.env;
+
+  // Validate required environment variables >>>>>>>>>>>
+  if (
+    !CLOUDFLARE_R2_ACCOUNT_ID ||
+    !CLOUDFLARE_R2_ACCESS_KEY_ID ||
+    !CLOUDFLARE_R2_SECRET_ACCESS_KEY
+  ) {
+    throw new Error("Missing Cloudflare R2 environment variables");
+  }
+  return new S3Client({
+    region: "auto",
+    endpoint: `https://${CLOUDFLARE_R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+      accessKeyId: CLOUDFLARE_R2_ACCESS_KEY_ID,
+      secretAccessKey: CLOUDFLARE_R2_SECRET_ACCESS_KEY,
+    },
+  });
+};
+export default s3Client;
+ 
