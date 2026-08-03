209859ae80160aff4e5bdb8cc95fcc6def0b2145
sona mittal <sonamittal285@gmail.com>
2026-08-03 12:55:11 +0530

    I

--- src/app/(pages)/panel/customers/page.tsx
+++ src/app/(pages)/panel/customers/page.tsx
@@ -1,54 +1,6 @@
-"use client";
-import { ConfigSettingDrawer } from "@/components/panel/layout/config-settings-drawer";
-import { Header } from "@/components/panel/layout/header";
-import { Main } from "@/components/panel/layout/main";
-import { ProfileDropdown } from "@/components/panel/layout/profile-dropdown";
-import { Search } from "@/components/panel/layout/search";
-import { ThemeSwitch } from "@/components/panel/layout/theme-switch";
-import CustomersTable from "@/components/website/pages/customers/customers-table";
-import AddCustomerDailog from "@/components/website/pages/customers/customers-dialog";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import { UserPlus } from "lucide-react";
+import Customers from "@/app/(pages)/panel/customers/customer";
 
-const Customers = () => {
-  const [Open, setOpen] = useState(false);
-
-  return (
-    <div className="flex flex-col min-h-screen">
-      {/* Header */}
-      <Header fixed>
-        <Search />
-        <div className="ms-auto flex items-center space-x-4">
-          <ThemeSwitch />
-          <ConfigSettingDrawer />
-          <ProfileDropdown />
-        </div>
-      </Header>
-
-      {/* Main content */}
-      <Main className="flex flex-1 flex-col gap-4 sm:gap-6">
-        <div className="flex flex-wrap items-end justify-between gap-2">
-          <div>
-            <h2 className="text-2xl font-bold tracking-tight">
-              Customers List
-            </h2>
-            <p className="text-muted-foreground">
-              Access and manage all your customer records, including contact and
-              financial details.
-            </p>
-          </div>
-          <Button onClick={() => setOpen(true)}>
-            <UserPlus className="mt-0.6 h-4 w-4" />
-            Add Customer
-          </Button>
-        </div>
-        {/* customers table */}
-        <CustomersTable />
-        {/* Customer btn */}
-        <AddCustomerDailog open={Open} onOpenChange={setOpen} />
-      </Main>
-    </div>
-  );
+const customerPage = () => {
+  return <Customers />;
 };
-export default Customers;
+export default customerPage;
--- src/app/(pages)/panel/invoices/page.tsx
+++ src/app/(pages)/panel/invoices/page.tsx
@@ -1,51 +1,6 @@
-"use client";
-import { ConfigSettingDrawer } from "@/components/panel/layout/config-settings-drawer";
-import { Header } from "@/components/panel/layout/header";
-import { Main } from "@/components/panel/layout/main";
-import { ProfileDropdown } from "@/components/panel/layout/profile-dropdown";
-import { Search } from "@/components/panel/layout/search";
-import { ThemeSwitch } from "@/components/panel/layout/theme-switch";
-import InvoiceTable from "@/components/website/pages/invoices/invoice-table";
-import AddInvoices from "@/components/website/pages/invoices/add";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import { Receipt } from "lucide-react";
-const Invoices = () => {
-  const [Open, setOpen] = useState(false);
+import Invoices from "@/app/(pages)/panel/invoices/invoice";
 
-  return (
-    <div className="flex flex-col min-h-screen">
-      {/* Header */}
-      <Header fixed>
-        <Search />
-        <div className="ms-auto flex items-center space-x-4">
-          <ThemeSwitch />
-          <ConfigSettingDrawer />
-          <ProfileDropdown />
-        </div>
-      </Header>
-
-      {/* Main content */}
-      <Main className="flex flex-1 flex-col gap-4 sm:gap-6">
-        <div className="flex flex-wrap items-end justify-between gap-2">
-          <div>
-            <h2 className="text-2xl font-bold tracking-tight">Invoices List</h2>
-            <p className="text-muted-foreground">
-              View and manage invoices with customer details, invoice items,
-              payment status, due dates, and total amounts.
-            </p>
-          </div>
-          <Button onClick={() => setOpen(true)}>
-            <Receipt className="mt-0.6 h-4 w-4" />
-            Add Invoices
-          </Button>
-        </div>
-        {/* invoice table */}
-        <InvoiceTable />
-        {/* invoices btn */}
-        <AddInvoices open={Open} onOpenChange={setOpen} />
-      </Main>
-    </div>
-  );
+const invoicespage = () => {
+  return <Invoices />;
 };
-export default Invoices;
+export default invoicespage;
